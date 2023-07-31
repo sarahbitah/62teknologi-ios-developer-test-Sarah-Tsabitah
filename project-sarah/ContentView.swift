e1c5b55c4970cd60c3206ec13a5b018580f4fcff
Sarah Tsabitah <tsabitsarah1@gmail.com>
2023-07-31 17:34:20 +0700

    Initial Commit

---
+++ project-sarah/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  project-sarah
+//
+//  Created by Sarah Tsabitah on 31/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ project-sarah/project_sarahApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_sarahApp.swift
+//  project-sarah
+//
+//  Created by Sarah Tsabitah on 31/07/23.
+//
+
+import SwiftUI
+
+@main
+struct project_sarahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
